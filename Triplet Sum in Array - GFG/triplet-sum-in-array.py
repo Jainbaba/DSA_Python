d4250215177bbc1e303a9aa0669650e771a8ac2d
Jainbaba <65411438+Jainbaba@users.noreply.github.com>
2023-01-31 13:15:13 +0530

    Added solution - LeetHub

---
+++ Triplet Sum in Array - GFG/triplet-sum-in-array.py
@@ -0,0 +1,52 @@
+#User function Template for python3
+class Solution:
+     
+    #Function to find if there exists a triplet in the 
+    #array A[] which sums up to X.
+    def find3Numbers(self,A, n, X):
+        A.sort()
+        for i in range(0,n):
+            sm = X - A[i]
+            l = i+1
+            h = n-1
+            while(l<h):
+                if A[l] + A[h] > sm:
+                    h -= 1
+                elif A[l] + A[h] < sm:
+                    l += 1
+                else:
+                    return 1
+        return 0
+
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import atexit
+import io
+import sys
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+@atexit.register
+
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+if __name__=='__main__':
+    t = int(input())
+    for i in range(t):
+        n,X=map(int,input().strip().split())
+        A=list(map(int,input().strip().split()))
+        ob=Solution()
+        if(ob.find3Numbers(A,n,X)):
+            print(1)
+        else:
+            print(0)
+# } Driver Code Ends
