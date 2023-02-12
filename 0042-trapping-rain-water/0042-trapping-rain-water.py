4886fdb857be5abd56a4a9599e0cf96e6742950c
Jainbaba <65411438+Jainbaba@users.noreply.github.com>
2023-02-12 14:57:27 +0530

    Time: 164 ms (28.04%), Space: 16.2 MB (15.73%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -5,9 +5,9 @@ class Solution:
         left = [height[0]]
         for i in range(1,n):
             left.append(max(height[i],left[-1]))
-            right.insert(0,max(height[n-1-i],right[0]))
+            right.append(max(height[n-1-i],right[-1]))
         units = 0
         for i in range(0,n):
-            units += min(left[i],right[i]) - height[i]
+            units += min(left[i],right[n-1-i]) - height[i]
         return units
         
